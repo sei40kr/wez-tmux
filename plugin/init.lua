7db6c498e6179879308868326192411786896f84
K H Soh <hju37823@outlook.com>
2024-10-18 10:27:00 +0800

    Fixed sending of leader key

--- plugin/init.lua
+++ plugin/init.lua
@@ -178,7 +178,9 @@ function M.apply_to_config(config, _)
   end
 
   local keys = {
-    { key = config.leader.key, mods = "LEADER|" .. config.leader.mods, action = act.SendKey({ key = config.leader, mods = config.leader.mods }) },
+    { key = config.leader.key,
+      mods = "LEADER|" .. config.leader.mods,
+      action = act.SendKey({ key = config.leader.key, mods = config.leader.mods }) },
 
     -- Workspaces
     { key = "$",               mods = "LEADER",                        action = M.action.RenameWorkspace },
